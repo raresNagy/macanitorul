0b2347f4f707988e9c38a4cdeb733c672d607ff9
raresNagy <rares.nagy@gmail.com>
2022-05-01 22:25:39 +0300

    merge sa moara mama

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // Imports
-import DiscordJS, { Intents} from 'discord.js'
+import { userMention } from '@discordjs/builders';
+import DiscordJS, { Intents, Interaction, InteractionCollector, Message, MessageAttachment, Options, User } from 'discord.js'
 import dotenv from 'dotenv'
 import fs from 'node:fs';
 dotenv.config()
@@ -21,6 +22,73 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('MAC MAC MAC')
+
+    const guildId = '816709720894472215'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild)
+        commands = guild.commands
+    else
+        commands = client.application?.commands
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong.',
+    })
+
+    commands?.create({
+        name: 'robot',
+        description: 'Vezi un robot Xeo din sezonul precizat',
+        options: [
+            {
+                name: 'sezon',
+                description: 'Numele sau numarul sezonului.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING
+            }
+        ]
+    })
+})
+
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options } = interaction
+    var FreightFrenzy = []
+    const FreightFrenzyFolder = fs.readdirSync('FreightFrenzy')
+
+    for (const file of FreightFrenzyFolder) {
+        FreightFrenzy.push('./FreightFrenzy/' + file)
+    }
+
+
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true,
+        })
+    }
+    else if (commandName === 'robot') {
+        const sezon = options.getString('sezon')
+        if (sezon === '6' || sezon === '2021' || sezon === 'Freight Frenzy') {
+            interaction.reply({
+                content: 'Uite https://imgur.com/a/gJRE1gs',
+                ephemeral: false,
+            })
+        }
+        else {
+            interaction.reply({
+                content: 'Sezonul acesta nu exista\n scrie numele, numarul, sau anul sezonului',
+                ephemeral: true,
+
+            })
+
+        }
+    }
 })
 
 

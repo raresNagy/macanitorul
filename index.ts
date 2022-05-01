ce46b188f2abe19570bdc811c34bc50f65626bed
raresNagy <rares.nagy@gmail.com>
2022-05-01 15:14:08 +0300

    took a break

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 // Imports
-import DiscordJS, { Intents } from 'discord.js'
+import DiscordJS, { Intents} from 'discord.js'
 import dotenv from 'dotenv'
-// Get token
+import fs from 'node:fs';
 dotenv.config()
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+    const command = require('./commands/${file}')
+    commands.push(command.data.toJSON());
+}
 
 const client = new DiscordJS.Client({
     intents: [
@@ -11,16 +18,28 @@ const client = new DiscordJS.Client({
     ]
 })
 
+
 client.on('ready', () => {
     console.log('MAC MAC MAC')
 })
 
+
 client.on('messageCreate', (message) => {
+    // Test
     if (message.content === 'mac?') {
         message.reply({
             content: 'MAC!'
         })
     }
+    // else if (message.content)
+})
+
+
+client.on('messageCreate', (message) => {
+    if (message.content.includes('auto'))
+        message.reply({
+            content: 'MAC!'
+        })
 })
 
 client.login(process.env.TOKEN)
